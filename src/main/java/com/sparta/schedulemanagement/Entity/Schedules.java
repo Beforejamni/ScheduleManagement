34f940639b2c54a8b4310cd1502f26b0114040ad
Beforejamni <beforejamn1@gmail.com>
2025-08-01 11:19:00 +0900

    feat : sql 개인 정보 gitignore 처리  1단계 과제 진행

--- src/main/java/com/sparta/schedulemanagement/Entity/Schedules.java
+++ src/main/java/com/sparta/schedulemanagement/Entity/Schedules.java
@@ -1,4 +1,27 @@
 package com.sparta.schedulemanagement.Entity;
 
-public class Schedules {
+
+import jakarta.persistence.*;
+import lombok.Getter;
+
+
+@Getter
+@Entity
+@Table(name = "schedules")
+public class Schedules extends BaseTimeEntity {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "longtext")
+    private String context;
+
+
+    @Column(nullable = false)
+    private String authorname;
 }
